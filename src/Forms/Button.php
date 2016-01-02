51b535b51cf2caa02475be6e2cac8fa6d15d4e35
ondrakoupil <koupil@optimato.cz>
2016-01-02 17:03:36 +0100

    Buttons with labels and icons

--- src/Forms/Button.php
+++ src/Forms/Button.php
@@ -3,16 +3,23 @@
 namespace OndraKoupil\Nette\Forms;
 
 /**
- * Univerzální formulářový prvek pro tlačítka s možností nastavit třídu a typ
+ * Univerzální formulářový prvek pro tlačítka s možností nastavit třídu a typ a případně ikonu.
+ *
+ * Při použití $labelIcon je nutné přidat styly z http://www.jasny.net/bootstrap/css/#buttons
  */
 
 class Button extends \Nette\Forms\Controls\Button {
 
-	function __construct($caption = NULL, $type = "button", $class = "btn") {
+	protected $labelIcon;
+	protected $labelIconLeft = true;
+
+	function __construct($caption = NULL, $type = "button", $class = "btn", $labelIcon = null, $labelIconOnLeft = true) {
 		parent::__construct($caption);
 		$proto = $this->getControlPrototype();
 		$proto->setName("button");
 		$proto->type($type);
+		$this->labelIcon = $labelIcon;
+		$this->labelIconLeft = $labelIconOnLeft ? true : false;
 		if ($class) $proto->addClass($class);
 	}
 
@@ -20,11 +27,21 @@ class Button extends \Nette\Forms\Controls\Button {
 		$control = parent::getControl($caption);
 
 		if ($control->value) unset($control->value);
-
 		if ($caption === null and $this->caption) $caption = $this->caption;
+
+		if ($this->labelIcon) {
+			if ($this->labelIconLeft) {
+				$caption = "<span class='btn-label'><i class='fa fa-".$this->labelIcon."'></i></span>" . $caption;
+			} else {
+				$caption .= "<span class='btn-label btn-label-right'><i class='fa fa-".$this->labelIcon."'></i></span>";
+			}
+			$control->class[] = "btn-labeled";
+		}
+
 		if (!$control->getHtml() and $caption) {
 			$control->setHtml($caption);
 		}
+
 		return $control;
 	}
 
